b5d7f4e8b4d4c113ca55449af96e8b5f94394f91
ProtoCyte <billyjia1@hotmail.com>
2024-01-01 01:02:02 -0800

    command logger made

--- bot.py
+++ bot.py
@@ -3,9 +3,6 @@ import discord
 import datetime
 from config import BOT_TOKEN
 
-
-
-
 default_activity = discord.Activity(type = discord.ActivityType.listening, name = "your every move")
 
 bot = commands.Bot(command_prefix="!",activity = default_activity, status = discord.Status.idle ,intents = discord.Intents.all())
@@ -27,8 +24,13 @@ async def currentstatus(ctx):
 # Deletes all messages in a channel (just for convenience)
 @bot.command()
 async def deleteall(ctx):
+    channel = ctx.channel.name
     await ctx.channel.purge()
+    await commandLogger("!deleteall", channel)
     
+async def commandLogger(command_name = str, channel_name = str):
+    channel = bot.get_channel(1190976583125700709)
+    await channel.send(f"successfully ran {command_name} in {channel_name}")
     
     
     
