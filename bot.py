54bab76d582c649d7c89dfe24ce6ac63f672bad7
ProtoCyte <billyjia1@hotmail.com>
2024-01-01 19:49:43 -0800

    committed everything

--- bot.py
+++ bot.py
@@ -12,21 +12,45 @@ async def on_ready():
     await bot.wait_until_ready()     
     print("Bot is ready")
 
-
-
-# This gets the bot's current status
+# This gets the bot's current status with no reply
 @bot.command()
 async def currentstatus(ctx):
     current_activity = bot.activity
-    print(f"Bot is currently {current_activity.name}")
+    await ctx.send(f"Bot is currently {default_activity.type.name} to {current_activity.name}")
+
+
+# This changes the bot's current status
+@bot.command()
+async def changestatus(ctx,status):
+    if (status == "idle"):
+        await bot.change_presence(status = discord.Status.idle)
+    elif (status == "online"):
+        await bot.change_presence(status = discord.Status.online)
+    elif (status == "offline"):
+        await bot.change_presence(status = discord.Status.offline)
+    elif (status == "dnd" | status == "do_not_disturb"):
+        await bot.change_presence(status = discord.Status.dnd)
+    elif (status == "invisible"):
+        await bot.change_presence(status = discord.Status.invisible) 
+    else: await ctx.send("Please choose a valid status")
+        
+# This changes what the bot activity is
+@bot.command()
+async def changeActivity(ctx, activity, activity_name):
     
+    
+
 # Deletes all messages in a channel (just for convenience)
 @bot.command()
 async def deleteall(ctx):
     channel = ctx.channel.name
     await ctx.channel.purge()
     await commandLogger("!deleteall", channel)
-    
+
+
+
+
+#Sends a message whenever a command is called, listing channel name and command
 async def commandLogger(command_name = str, channel_name = str):
     channel = bot.get_channel(1190976583125700709)
     await channel.send(f"successfully ran {command_name} in {channel_name}")
--- classes/BotStatus.py
+++
@@ -1,6 +0,0 @@
-# from dataclasses import dataclass
-
-# @dataclass
-# class DataBotStatus:
-#     current_status: str = "Nothing"
-#     current_activity: str = "Chilling"
